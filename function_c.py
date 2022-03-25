889c925825e004450b5bd2f9c475c99ee82ed4fe
Danielle <dmbecker24@gmail.com>
2022-03-25 13:43:52 -0700

    changed function_c

--- function_c.py
+++ function_c.py
@@ -3,8 +3,7 @@ def merge_lists(list_a, list_b):
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
-
-
+    return list(set(list_a) | set(list_b))
+    
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
