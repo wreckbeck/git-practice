1d02221ac0c6a96e1222987e2c327ff5421cdadb
Yonese James <yoneseguerrier@gmail.com>
2022-03-25 16:47:12 -0400

    Entered code for function_d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,14 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+
+    #This is Yonese's code change
+    max_numbers = numbers[0]
+    for num in numbers:
+        if num > max_number:
+            max_num = num 
+    return max
+
 
 
 if __name__ == "__main__":
